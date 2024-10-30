4e5f49a0890dd2b84158da3e0d50fdd1e0a2b684
Alexandre Pron <alexandre.pron@gmail.com>
2024-10-30 16:02:29 +0100

    [BF]: added r string to deal with white spaces in path

--- shanoir2bids.py
+++ shanoir2bids.py
@@ -652,7 +652,7 @@ Search Text : "{}" \n""".format(
                 self.export_dcm2niix_config_options(dcm2niix_config_file.name)
                 workflow_params = {
                     "files": glob(opj(tmp_dicom, "*", "*.dcm"), recursive=True),
-                    "outdir": opj(self.dl_dir, str(self.shanoir_study_id)),
+                    "outdir": repr(opj(self.dl_dir, self.shanoir_study_id)),
                     "subjs": [subject_id],
                     "converter": "dcm2niix",
                     "heuristic": heuristic_file.name,
